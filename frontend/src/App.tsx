191a2b24df032866703d8399231319cfae10b420
tomas <tomasbeyene21@gmail.com>
2023-06-08 08:14:31 +0300

    read

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,22 +2,41 @@ import React, { ChangeEvent, useState } from "react";
 import "./App.css";
 import * as XLSX from "xlsx";
 
+// interface Item {
+//   [key: string]: string | number;
+// }
+
+// interface SubCategory {
+//   subCategory: string;
+//   items: Item[];
+// }
+
+// interface Category {
+//   category: string;
+//   subCategories: SubCategory[];
+// }
+
 interface Item {
-  [key: string]: string | number;
+  itemNo: string;
+  description: string;
+  unit: string;
+  qty: string | number;
+  rate: string | number;
+  amount: string | number;
 }
 
 interface SubCategory {
-  subCategory: string;
+  name: string;
   items: Item[];
 }
 
 interface Category {
-  category: string;
+  name: string;
   subCategories: SubCategory[];
 }
 
 const App: React.FC = () => {
-  const [data, setData] = useState<Category[]>([]);
+  // const [data, setData] = useState<Category[]>([]);
 
   const handleFileUpload = (e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
@@ -30,63 +49,35 @@ const App: React.FC = () => {
           const wsname = wb.SheetNames[0];
           const ws = wb.Sheets[wsname];
           const sheetData = XLSX.utils.sheet_to_json(ws, { header: 1 });
-          const jsonData: Category[] = [];
-          let currentCategory = "";
-          let currentSubCategory = "";
-          let headers: string[] = [];
-          console.log(sheetData);
-          console.log("sheetData");
 
-          for (let i = 0; i < sheetData.length; i++) {
-            console.log(sheetData[i]);
-            // console.log("sheetData[i]");
-            const row: (string | number)[] = sheetData[i];
+          const categories: Category[] = [];
+          let column: (string | number)[] = [];
 
-            if (
-              row[1] &&
-              ((row[1] as string)?.startsWith("A.") ||
-                (row[1] as string)?.startsWith("B."))
-            ) {
-              currentCategory = row[0] as string;
-              jsonData.push({ category: currentCategory, subCategories: [] });
-            } else if (row[1]) {
-              currentSubCategory = row[1] as string;
+          // let currentCategory: Category | null = null;
+          // let currentSubCategory: SubCategory | null = null;
 
-              // Check if the last element in jsonData exists before pushing to its subCategories
-              if (jsonData[jsonData.length - 1]) {
-                jsonData[jsonData.length - 1].subCategories.push({
-                  subCategory: currentSubCategory,
-                  items: [],
-                });
-              }
-
-              headers = row.slice(2) as string[]; // Store headers for items
-            } else if (row[2]) {
-              const item: Item = {};
-              headers.forEach((header, index) => {
-                item[header] = row[index + 2];
-              });
-
-              // Check if the last element in jsonData and its subCategories exist before pushing the item
-              if (
-                jsonData[jsonData.length - 1] &&
-                jsonData[jsonData.length - 1].subCategories[
-                  jsonData[jsonData.length - 1].subCategories.length - 1
-                ]
-              ) {
-                jsonData[jsonData.length - 1].subCategories[
-                  jsonData[jsonData.length - 1].subCategories.length - 1
-                ].items.push(item);
-              }
+          for (const row of sheetData as (string | number)[][]) {
+            // console.log(row);
+            if (row.length === 0) {
+              continue;
             }
-            console.log("jsonData");
 
-            console.log(jsonData);
-            console.log("ooooooooo");
+            if (row[0] === "Item No ") {
+              column = row;
+              console.log(column);
+              continue;
+            }
+            console.log(row[1]);
+            if ((row[1] as string)?.startsWith("A." || " A.")) {
+              const category: Category = {
+                name: row[1] as string,
+                subCategories: [],
+              };
+              categories.push(category);
+              // console.log(category);
+              continue;
+            }
           }
-
-          setData(jsonData);
-          // console.log(jsonData);
         }
       };
       reader.readAsBinaryString(file);
@@ -102,22 +93,50 @@ const App: React.FC = () => {
 
 export default App;
 
+// [1]
+// [1.1]
+// [1.1.1, a,b,c]
+// [1.1.2, a,b,c]
+// [1.1.3, a,b,c]
 
+// [1]
+// [1.1]
+// [1.1.1, a,b,c]
+// [1.1.2, a,b,c]
+// [1.1.3, a,b,c]
 
-1 
+// {
+//   category: 1,
+//   subCategories: [
+//     {
+//       subCategory: 1.1,
+//       items: [
+//         {
+//           item: 1.1.1,
+//           a: a,
+//           b: b,
+//           c: c
+//         },
+//         {
+//           item: 1.1.2,
+//           a: a,
+//           b: b,
+//           c: c
+//         },
+//         {
+//           item: 1.1.3,
+//           a: a,
+//           b: b,
+//           c: c
 
-1.1 
+// }
 
-1.1.1 
-
-1.1.2 
-
-1.1.3
-
-1.2
-
-1.2.1
-
-1.2.2
-
-1.2.3
+// ['Item No ', 'Description', 'Unit ', 'Qty ', 'Rate ', 'Amount ']
+// [empty, ' A. Substructure']
+// [empty, '1. Excavation and Earth Works ']
+// [1.1, 'Clearing of Ordinary Soil, Root and a made up Grou…average depth of 20 cm from natural ground level ', 'm2', 840, 18, 15120]
+// [1.2, 'Bulk excavation in an ordinary soil  to a depth not exceeding  150 cm from  Cleared Level', 'm3', 1247.085, 122, 152144.37]
+// [1.3, 'Ditto item No. 1.2.  but  exceeding 150 cm But not exceeding 300 cm from Cleared Level', 'm3', 1247.085, 122, 152144.37]
+// [1.4, 'Ditto item No. 1.2.  but exceeding 300 cm but not exceeding 525 cm from cleared level', 'm3', 872.9594999999999, 218, 190305.17099999997]
+// [1.5, 'Excavation in weathered rock with different depth', 'm3', '-', 701, '-']
+// [1.6, 'Ditto but in a Strong Basaltic rock of different depth', 'm3', '-', 701, '-']
